b890f95cae6f954d72919fd874178a9e38980a3b
Manish <manishgupta1998mus@gmail.com>
2023-02-26 23:17:05 +0530

    Vehicles it is

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -1,23 +1,20 @@
 package com.driver;
 
-public class Boat implements WaterVehicle{
+public class Boat implements WaterVehicle {
 
     private String name;
     private int capacity;
 
-    public String getName() {
-        return name;
+    public Boat(String name, int capacity){
+        this.name = name;
+        this.capacity = capacity;
     }
 
-    public void setName(String name) {
+    public void setName(String name){
         this.name = name;
     }
 
-    public int getCapacity() {
-        return capacity;
-    }
-
-    public void setCapacity(int capacity) {
+    public void setCapacity(int capacity){
         this.capacity = capacity;
     }
 
@@ -30,4 +27,5 @@ public class Boat implements WaterVehicle{
     public int getVehicleCapacity() {
         return this.capacity;
     }
+
 }
